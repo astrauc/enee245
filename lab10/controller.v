834d182dde66bff63329f13d11f7995e526a773e
Anson <a1strauch@yahoo.com>
2025-04-21 19:33:29 -0400

    eee

--- lab10/controller.v
+++ lab10/controller.v
@@ -1,1 +1,24 @@
-git branch --set-upstream-to=origin/<branch> fartsmella
+ always @(start) begin 
+   if (start) begin 
+      ld_add_p = 0;
+      ld_shift_a = 0;
+      ld_shift_b = 0;
+      en_a = 1;
+      en_b = 1;
+      en_p = 1;
+   end else begin 
+      if (zero) begin 
+         en_a = 0;
+         en_b = 0;
+         en_p = 0;
+      end else begin 
+         ld_shift_a = 1;
+         ld_shift_b = 1;
+         if (lsb_b) begin 
+            ld_add_p = 1;
+         end else begin 
+            ld_add_p = 0;
+         end 
+      end
+   end 
+ end
