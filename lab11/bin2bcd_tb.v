1dd5a531f177f6c042464a6fc2ca59426e8d4d57
Anson <a1strauch@yahoo.com>
2025-04-28 18:44:58 -0400

    testbenches

--- lab11/bin2bcd_tb.v
+++ lab11/bin2bcd_tb.v
@@ -1,6 +1,30 @@
-module bin2bcd_tb();
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/28/2025 05:05:26 PM
+// Design Name: 
+// Module Name: bin2bcd_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-   reg [7:0] bin;
+
+module bin2bcd_tb(
+
+    );
+    
+    reg [7:0] bin;
    wire [11:0] bcd;
    integer i;
    bin2bcd uut(.bin(bin), .bcd(bcd));
@@ -13,5 +37,4 @@ module bin2bcd_tb();
          $display("Bin = %d, BCD = %d, %d, %d", bin, bcd[11:8], bcd[7:4], bcd[3:0]);
       end
    end
-
 endmodule
